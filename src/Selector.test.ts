44ab46c0fc4cc06b68baf2882960cddf3e5e5968
Adam Due Hansen <adamduehan@gmail.com>
2020-11-11 14:32:21 +0100

    Adds withTagname.

--- src/Selector.test.ts
+++ src/Selector.test.ts
@@ -1,14 +1,41 @@
-import createSeletor from "./selector";
+/**
+ *
+ * API
+ * createSelector().withTagname('div').selector           => div
+ * createSelector().withId('any-id).selector              => #any-id
+ * createSelector().withCssClass('any-class').selector    => .any-class
+ * createSelector()
+ *  .withTagname('div')
+ *  .and()
+ *  .withId('any-id')
+ *  .selector                                             => div#any-id
+ *
+ */
 
-describe("createSelector", () => {
-  test("should have empty selector", () => {
+import createSeletor from './selector';
+
+describe('createSelector', () => {
+  test('should have empty selector', () => {
     // Arrange
-    const expectedSelector = "";
+    const expectedSelector = '';
 
     // Act
     const actualSelector = createSeletor().selector;
 
     // Assert
     expect(actualSelector).toEqual(expectedSelector);
+  });
+
+  describe('withTagname', () => {
+    test('should set tagname in selector', () => {
+      // Arrange
+      const expectedSelector = 'div';
+
+      // Act
+      const actualSelector = createSeletor().withTagname('div').selector;
+
+      // Assert
+      expect(actualSelector).toEqual(expectedSelector);
+    });
   });
 });
--- src/Selector.ts
+++ src/Selector.ts
@@ -1,6 +1,40 @@
+interface HtmlElementTagName {
+  div: HTMLDivElement;
+}
+
+interface Select {
+  tagname: string;
+}
+
 class Selector {
+  private selectors: Array<Select> = [
+    {
+      tagname: '',
+    },
+  ];
+
   get selector(): string {
-    return '';
+    const selectors = this.selectors.map((selector) => {
+      const { tagname } = selector;
+      return `${tagname}`;
+    });
+    return selectors.join(',');
+  }
+
+  withTagname(tag: keyof HtmlElementTagName): Selector {
+    this.selectors = this.selectors.map(
+      (selector, index): Select => {
+        if (index + 1 !== this.selectors.length) {
+          return selector;
+        } else {
+          return {
+            ...selector,
+            tagname: tag,
+          };
+        }
+      }
+    );
+    return this;
   }
 }
 
